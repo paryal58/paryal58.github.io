c1a179cbab26eb3650882374e2484d8d9e946673
paryal58 <92784787+paryal58@users.noreply.github.com>
2023-09-18 22:15:43 -0400

    paryal58.github.io: More refinement to the site

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-const scrollingText = document.querySelector('.scrolling-text');
-const scrollingContainer = document.querySelector('.scrolling-container');
+const menuButton = document.getElementById("menu-button");
+const hiddenMenu = document.getElementById("hidden-menu");
 
-scrollingContainer.addEventListener('scroll', () => {
-    // Calculate the new font size based on the scroll position
-    const newSize = 93 - scrollingContainer.scrollTop * 0.1;
-    
-    // Set the new font size with a minimum of 16px
-    scrollingText.style.fontSize = Math.max(16, newSize) + 'px';
+menuButton.addEventListener("click", () => {
+    if (hiddenMenu.style.display === "block") {
+        hiddenMenu.style.display = "none";
+    } else {
+        hiddenMenu.style.display = "block";
+    }
 });
